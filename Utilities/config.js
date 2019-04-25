aabeb19595dba0c7968507308a72a44ad1c0fc07
Sumit ruhela <techugogithub>
2019-04-25 11:22:51 +0530

    change 25 april

--- Utilities/config.js
+++ Utilities/config.js
@@ -36,7 +36,7 @@ let serverURLs = {
     },
     "live": {
         "NODE_SERVER": "http://localhost",
-        "NODE_SERVER_PORT": "5050",
+        "NODE_SERVER_PORT": "6262",
         "MONGO_DB": "mongodb://35.165.140.114:27017/WAKINEW",
         "EMAIL_USER": 'toothfairysanjeet@gmail.com',
         "EMAIL_PASS": 'Sanj1234A',
